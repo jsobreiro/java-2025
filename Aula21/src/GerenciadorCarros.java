d5715e60d156c667d906940fb932faee4051e49e
Jason Antonio Pedroso Sobreiro <jasobreiro@up.edu.br>
2025-05-26 11:27:54 -0400

    Aula 21 - adicionado array list e método para remover carros da lista e do arquivo

--- Aula21/src/GerenciadorCarros.java
+++ Aula21/src/GerenciadorCarros.java
@@ -5,32 +5,90 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciadorCarros {
 
     private static final String ARQUIVO  = "carros.txt";
+    private static List<Carro> listaCarros;
+
+    public GerenciadorCarros() {
+        listaCarros = new ArrayList<>();
+    }
 
     public void adicionarCarro(Carro c) throws IOException {
 
         try (PrintWriter pw =   new PrintWriter(new BufferedWriter(new FileWriter(ARQUIVO, true)))) {
             pw.println(c.toTxt());
+            System.out.println("\nCarro cadastrado com sucesso!\n");
         } 
+        lerArquivo();
     }
 
     public void listarCarros() throws  IOException {
+
+        if (listaCarros.isEmpty()) {
+            System.out.println("\nATENÇÃO: Não há carros cadastrados!");
+            return;
+        }
+
+        System.out.println("\nCarros Cadastrados:\n");
+
+        for (Carro temp : listaCarros) {
+            System.out.println(temp);
+        }
+        
+
+    }
+
+    public void lerArquivo() throws IOException {
 
         try (BufferedReader br = new BufferedReader(new FileReader(ARQUIVO))) {
 
             String carroAtual;
 
+            listaCarros.clear(); // limpar a lista antes de ler o arquivo
+
             while((carroAtual = br.readLine()) != null) {
 
-                Carro carro = Carro.fromTxt(carroAtual);
-                System.out.println(carro);
-
+                // Cada linha do arquivo será convertida num objeto de Carro, que por sua vez,
+                // será adiciomado no arraylist
+                listaCarros.add(Carro.fromTxt(carroAtual));
             }
 
         }
+
+    }
+
+    public void removerCarro(String placa) throws IOException {
+
+        boolean encontrou = false;
+
+        for (Carro temp : listaCarros) {
+
+            if (temp.getPlaca().equals(placa)) {
+                listaCarros.remove(temp);
+                encontrou = true;
+                break;
+            }
+
+        }
+
+        if (!encontrou) {
+            System.out.println("Carro com a placa " + placa + " nao cadastrado!");
+            return;
+        }
+
+        try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(ARQUIVO, false)))){
+
+            for (Carro temp : listaCarros) {
+                pw.println(temp.toTxt());
+            }
+            System.out.println("\nCarro com a placa " + placa + " removido com sucesso!");
+
+        }
+
 
     }
 
--- Aula21/src/Principal.java
+++ Aula21/src/Principal.java
@@ -8,11 +8,18 @@ public class Principal {
         String modelo, placa, cor;
         int op;
 
+        try {
+            gc.lerArquivo();
+        } catch (IOException e) {
+            System.out.println("\nErro ao ler arquivo!");
+        }
+
         do { 
             
             System.out.println("\nGERENCIADOR DE CARROS");
             System.out.println("1) Cadastrar Carro");
             System.out.println("2) Listar Carros cadastrados");
+            System.out.println("3) Remover carro");
             System.out.println("0) Sair");
             System.out.print("Informe uma opção: ");
             op = Console.lerInt();
@@ -32,8 +39,7 @@ public class Principal {
 
                     try {
                         gc.adicionarCarro(new Carro(modelo, placa, cor));
-                        System.out.println("\nCarro cadastrado com sucesso!");
-
+                      
                     } catch (IOException e) {
                         System.out.println("Erro ao gravar no arquivo de texto:");
                         e.printStackTrace(); 
@@ -44,12 +50,25 @@ public class Principal {
                 case 2:
 
                     try {
-                        System.out.println("\nListando Carros cadastrados:");
                         gc.listarCarros();
                     } catch (IOException e) {
                         System.out.println("Erro ao ler arquivo de texto:");
                         e.printStackTrace();       
                     }  
+
+                    break;
+
+                case 3:
+
+                    System.out.println("\nInforme a placa do carro que deseja remover: ");
+                    placa = Console.lerString();
+
+                    try {
+                        gc.removerCarro(placa);
+                    } catch (IOException e) {
+                        System.out.println("\nErro ao atualizar o arquivo:");
+                        e.printStackTrace();
+                    }
 
                     break;
 
@@ -60,6 +79,7 @@ public class Principal {
 
                 default:
                     System.out.println("\nOpção inválida! digite novamente.");
+                    break;
             }
 
         } while (op != 0);
